9339ccd8c8392a655bb42487a8650dff50811aad
Masom <Masom@DESKTOP-GPCOC4B>
2025-12-15 18:53:48 +0000

    Add project files.

---
+++ Testing Number 1/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Testing_Number_1.Models;
+using System.Collections.Generic;
+
+
+namespace Testing_Number_1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<TodoItem> TodoItems { get; set; }
+
+        public void OnGet()
+        {
+            TodoItems = new List<TodoItem>
+{
+    new TodoItem { Id = 1, Title = "Learn ASP.NET", IsCompleted = false },
+    new TodoItem { Id = 2, Title = "Build ToDo App", IsCompleted = false }
+};
+
+        }
+    }
+}
---
+++ Testing Number 1/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("addTodoBtn");
+    const input = document.getElementById("todoInput");
+
+    if (button) {
+        button.addEventListener("click", function () {
+            const value = input.value;
+            alert("You typed: " + value);
+        });
+    }
+});
